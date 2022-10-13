43641a5cc4d3750d199bf80ac1d748aff756d8ec
mtsadoremos <mtsadoremos@gmail.com>
2022-10-14 00:43:34 +0800

    added profile picture

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), image()],
+  output: "server",
+  adapter: netlify()
 });
